fc7d327ca79630e45e1889a800f7b097362dc8cd
RafayelGardishyan <rgardishyan@gmail.com>
2018-01-04 16:57:35 +0100

    Update

--- Calendar/views.py
+++ Calendar/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.template import loader
 
 
 def index(request):
-    template = loader.get_template('calendar/index.html')
-    context = {
+    if request.session.get('logged_in'):
+        template = loader.get_template('calendar/index.html')
+        context = {
 
-    }
-    return HttpResponse(template.render(context, request))
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        return redirect('/')
