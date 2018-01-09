e32b0daca263dec9ed92221286cb9ae39ffb8cc6
RafayelGardishyan <rgardishyan@gmail.com>
2018-01-09 18:10:40 +0100

    Added shortener fix

--- Shortener/views.py
+++ Shortener/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.template import loader
-
+import requests as rq
 from .models import Short
 from .forms import ShortForm
 from webhooks import Webhook
@@ -19,36 +19,47 @@ def index(request):
             # create a form instance and populate it with data from the request:
             form = ShortForm(request.POST)
             # check whether it's valid:
+
             if form.is_bound:
                 if form.is_valid():
+                    if rq.get('https://codename-codeniacs.herokuapp.com/s/' + form.cleaned_data['path']).status_code != 200:
+                        form.save()
 
-                    form.save()
+                        template = loader.get_template('error.html')
+                        context = {
+                            'message': 'Added Short codename-codeniacs.herokuapp.com/s/' + form.cleaned_data['path'] + ' For Url ' + form.cleaned_data['url'],
+                            'link': {
+                                'text': 'Add another short',
+                                'url': '/s'
+                            },
+                            'slink': {
+                                'text': 'Visit Website',
+                                'url': form.cleaned_data['url']
+                            },
+                        }
+                        embed = Webhook(values['whurl'], color=123123)
 
-                    template = loader.get_template('error.html')
-                    context = {
-                        'message': 'Added Short codename-codeniacs.herokuapp.com/s/' + form.cleaned_data['path'] + ' For Url ' + form.cleaned_data['url'],
-                        'link': {
-                            'text': 'Add another short',
-                            'url': '/s'
-                        },
-                        'slink': {
-                            'text': 'Visit Website',
-                            'url': form.cleaned_data['url']
-                        },
-                    }
-                    embed = Webhook(values['whurl'], color=123123)
+                        embed.set_author(name='Codeniacs Website',
+                                         icon='https://codename-codeniacs.herokuapp.com/static/favicon.png')
+                        embed.set_desc('Added Shortcut')
+                        embed.add_field(name='Path', value=form.cleaned_data['path'])
+                        embed.add_field(name='Url', value=form.cleaned_data['url'])
+                        embed.set_thumbnail('https://codename-codeniacs.herokuapp.com/static/favicon.png')
 
-                    embed.set_author(name='Codeniacs Website',
-                                     icon='https://codename-codeniacs.herokuapp.com/static/favicon.png')
-                    embed.set_desc('Added Shortcut')
-                    embed.add_field(name='Path', value=form.cleaned_data['path'])
-                    embed.add_field(name='Url', value=form.cleaned_data['url'])
-                    embed.set_thumbnail('https://codename-codeniacs.herokuapp.com/static/favicon.png')
-
-                    embed.set_footer(text='This message was automatically sent form Codeniacs Website',
-                                     icon='https://codename-codeniacs.herokuapp.com/static/favicon.png', ts=True)
-                    embed.post()
-                    return HttpResponse(template.render(context, request))
+                        embed.set_footer(text='This message was automatically sent form Codeniacs Website',
+                                         icon='https://codename-codeniacs.herokuapp.com/static/favicon.png', ts=True)
+                        embed.post()
+                        return HttpResponse(template.render(context, request))
+                    else:
+                        template = loader.get_template('error.html')
+                        context = {
+                            'message': 'This short already exists',
+                            'link': {
+                                'text': 'Return to Shortener home',
+                                'url': '/s'
+                            }
+                        }
+                        return HttpResponse(template.render(context, request))
                 else:
                     template = loader.get_template('error.html')
                     context = {
